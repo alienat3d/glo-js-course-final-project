6ae520e76ec3671d007d2044f01653a9a70bbb02
Al Zaplin <zaplin.dev@gmail.com>
2024-09-12 16:32:33 +0300

    feat(slider) 1.10: Мобильная версия секции «Формула успешности ремонта» в виде слайдера, который листается в ту или иную сторону бесконечно

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { modifyPhoneInputsFunc } from "./modules/modify-phone-inputs";
 import { sendFormFunc } from "./modules/send-form";
 import { privacyPopupFunc } from "./modules/privacy-popup";
 import { formulaTooltipsFunc } from "./modules/formula-tooltips";
+import { sliderFunc } from "./modules/slider";
 
-headerPhonesAccordionFunc();
-headerMenuRolloutFunc();
-smoothScrollToFunc();
-servicesPopupFunc();
-modifyPhoneInputsFunc();
-sendFormFunc();
-privacyPopupFunc();
-formulaTooltipsFunc();
+// headerPhonesAccordionFunc();
+// headerMenuRolloutFunc();
+// smoothScrollToFunc();
+// servicesPopupFunc();
+// modifyPhoneInputsFunc();
+// sendFormFunc();
+// privacyPopupFunc();
+// formulaTooltipsFunc();
+sliderFunc('.formula-slider-wrap', '.formula-item');
+
+// window.addEventListener("DOMContentLoaded", () => console.warn('Website is loaded.'));
---
+++ src/modules/slider.js
@@ -0,0 +1,63 @@
+export const sliderFunc = (containerClass, slideClass) => {
+  if (!containerClass || !slideClass) return;
+
+  const sliderBlock = document.querySelector(containerClass);
+  if (sliderBlock === null) return;
+  
+  const slides = sliderBlock.querySelectorAll(slideClass);
+  const sliderWrapper = sliderBlock.querySelector('.formula-slider');
+
+  let currentSlide = 0;
+
+  const showHideItems = (sliderContainer, slides, currentIdx) => {
+    sliderWrapper.innerHTML = '';
+    const visibleSlides = [];
+    if (currentIdx === 0) {
+      slides.forEach((slide, idx, array) => {
+        slide.classList.remove('active-item');
+        if (currentIdx === 0 && (idx === currentIdx || idx === array.length - 1 || idx === currentIdx + 1)) {
+          array[currentIdx].classList.add('active-item');
+          visibleSlides.push(slide);
+        }
+      })
+      visibleSlides.splice(0, 1, visibleSlides.splice(1, 1, visibleSlides[0])[0]);
+      visibleSlides.splice(2, 1, visibleSlides.splice(0, 1, visibleSlides[2])[0]);
+      visibleSlides.forEach(slide => sliderContainer.insertAdjacentElement('beforeend', slide));
+    } else if (currentIdx === slides.length - 1) {
+      slides.forEach((slide, idx, array) => {
+        slide.classList.remove('active-item');
+        if (idx === currentIdx || idx === currentIdx - 1 || idx === 0) {
+          array[currentIdx].classList.add('active-item');
+          visibleSlides.push(slide);
+        }
+      })
+      visibleSlides.splice(1, 1, visibleSlides.splice(2, 1, visibleSlides[1])[0]);
+      visibleSlides.splice(0, 1, visibleSlides.splice(2, 1, visibleSlides[0])[0]);
+      visibleSlides.forEach(slide => sliderContainer.insertAdjacentElement('beforeend', slide));
+    } else if (currentIdx > 0 || currentIdx < array.length - 2) {
+      slides.forEach((slide, idx, array) => {
+        slide.classList.remove('active-item');
+        if (idx === currentIdx || idx === currentIdx - 1 || idx === currentIdx + 1) {
+          array[currentIdx].classList.add('active-item');
+          sliderContainer.insertAdjacentElement('beforeend', slide);
+        }
+      })
+    }
+  }
+
+  sliderBlock.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (!evt.target.closest('.slider-arrow')) return;
+    if (evt.target.closest('.slider-arrow_left')) {
+      currentSlide--;
+    } else if (evt.target.closest('.slider-arrow_right')) {
+      currentSlide++;
+    }
+    if (currentSlide >= slides.length) currentSlide = 0;
+    if (currentSlide < 0) currentSlide = slides.length - 1;
+
+    showHideItems(sliderWrapper, slides, currentSlide);
+  })
+
+  showHideItems(sliderWrapper, slides, currentSlide);
+}
