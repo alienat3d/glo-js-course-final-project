b27abb4c20495cbccb44186d375966e58d006338
Al Zaplin <zaplin.dev@gmail.com>
2024-10-02 16:06:59 +0300

    feat(Services Popup) 1.21: Получение данных из базы данных JSON и дальнейшей заполнение контента с помощью них + сайдбар с кнопками; + текущий вид работ передаётся и в заголовок; + последнее время обновление берётся из текущей даты;

--- src/modules/services-popup-render-content.js
+++ src/modules/services-popup-render-content.js
@@ -5,6 +5,10 @@ export const servicesPopupRenderContentFunc = () => {
   const navSidebarContainer = document.querySelector('.nav-list-popup-repair');
   const serviceTypeTitle = document.querySelector('.popup-repair-types-content__head-title');
   const dateBlock = document.querySelector('.popup-repair-types-content__head-date span');
+  const contentContainer = document.querySelector('.popup-repair-types-content-table__list tbody');
+
+  let activeButton;
+  let type;
 
   const getData = () => {
     return fetch(SERVER_URL)
@@ -27,14 +31,51 @@ export const servicesPopupRenderContentFunc = () => {
         <button class="button_o popup-repair-types-nav__item">${type}</button>
       `)
     );
-    const activeButton = navSidebarContainer.querySelector('.popup-repair-types-nav__item');
+
+    activeButton = navSidebarContainer.querySelector('.popup-repair-types-nav__item');
     activeButton.classList.add('active');
+
     serviceTypeTitle.textContent = activeButton.textContent;
+  }
+
+  const renderContent = (data, currentType) => {
+    data.forEach(obj => {
+      if (obj.type === currentType) {
+        contentContainer.insertAdjacentHTML('beforeend', `
+          <tr class="mobile-row showHide">
+            <td class="repair-types-name">${obj.name}</td>
+            <td class="mobile-col-title tablet-hide desktop-hide">Ед.измерения</td>
+            <td class="mobile-col-title tablet-hide desktop-hide">Цена за ед.</td>
+            <td class="repair-types-value">${obj.units}</td>
+            <td class="repair-types-value">${obj.cost} руб.</td>
+          </tr>
+        `)
+      }
+    });
   }
 
   getData().then((data) => renderSidebar(data));
 
+  getData().then((data) => {
+    type = data[0].type;
+    renderContent(data, type);
+  });
+
   dateBlock.textContent = new Intl.DateTimeFormat('ru',
     { year: 'numeric', month: 'long', day: 'numeric' })
-      .format(date);
+    .format(date);
+
+  navSidebarContainer.addEventListener('click', (evt) => {
+    const tgt = evt.target;
+    if (tgt.matches('.button_o') && !tgt.matches('.active')) {
+      const buttons = navSidebarContainer.querySelectorAll('.button_o');
+      buttons.forEach(button => {
+        if (button.classList.contains('active')) button.classList.remove('active');
+      });
+      tgt.classList.add('active');
+      contentContainer.innerHTML = '';
+      type = tgt.textContent;
+      getData().then((data) => renderContent(data, type));
+    }
+  })
 }
