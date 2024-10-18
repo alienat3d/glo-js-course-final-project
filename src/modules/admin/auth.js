385acbca80eb3f52b62e82600e39c88b33c772f6
Al Zaplin <zaplin.dev@gmail.com>
2024-10-18 22:44:43 +0300

    feat(auth) 2.3: Сверка введённых данных пользователем с базой данных

--- src/modules/admin/auth.js
+++ src/modules/admin/auth.js
@@ -12,25 +12,31 @@ export const authFunc = () => {
     const passwordWarning = passwordInput.nextElementSibling;
     const submitButton = document.querySelector('button[type=submit]');
 
-    // todo Надо сделать так, чтобы только при совпадении логина и пароля из одного и того же объекта проверка пропускала.
-    const checkUsernames = (data) => {
-      data['users'].forEach(obj => {
-        if (obj['login'] === usernameInputValue) isLoginCorrect = true;
-      });
-    }
-
-    const checkPasswords = (data) => {
-      data['users'].forEach(obj => {
-        if (obj['password'] === passwordInputValue) isPasswordCorrect = true;
-      });
-    }
-
     let isLoginCorrect = false;
-    let isPasswordCorrect = false;
     let isAuth = false;
 
     let usernameInputValue;
     let passwordInputValue;
+
+    const checkUsernames = (data) => {
+      if (data.users[0].login === usernameInputValue) {
+        isLoginCorrect = true;
+      } else {
+        usernameWarning.classList.remove('hidden');
+        usernameInput.value = '';
+      };
+    }
+
+    const checkPasswords = (data) => {
+      if (data.users[0].password === passwordInputValue && isLoginCorrect) {
+        document.cookie = `auth=true;samesite=strict;max-age=86400000`;
+        isAuth = true;
+        window.location.href = `http://${currentHost}/admin/table.html`;
+      } else {
+        passwordWarning.classList.remove('hidden');
+        passwordInput.value = '';
+      };
+    }
 
     const getCookie = () => {
       return document.cookie.split('; ').reduce((acc, item) => {
@@ -45,51 +51,6 @@ export const authFunc = () => {
     const checkCredentials = () => {
       getData(SERVER_URL).then((data) => checkUsernames(data));
       getData(SERVER_URL).then((data) => checkPasswords(data));
-
-      if (isLoginCorrect && isPasswordCorrect) {
-        console.log('Logged in!');
-        // document.cookie = `auth=true;samesite=strict;max-age=2678400`;
-        // isAuth = true;
-        // window.location.href = `http://${currentHost}/admin/table.html`;
-      } else if (!isLoginCorrect && !isPasswordCorrect) {
-          usernameWarning.classList.remove('hidden');
-          passwordWarning.classList.remove('hidden');
-          usernameInput.value = '';
-          passwordInput.value = '';
-      } else if (!isLoginCorrect) {
-        usernameWarning.classList.remove('hidden');
-        usernameInput.value = '';
-      } else if (!isPasswordCorrect) {
-        passwordWarning.classList.remove('hidden');
-        passwordInput.value = '';
-      }
-
-      /* switch (true) {
-        case getData(SERVER_URL).then((data) =>
-          checkObjectValues(data, 'login', usernameInputValue)) &&
-          getData(SERVER_URL).then((data) =>
-            checkObjectValues(data, 'password', passwordInputValue)):
-          document.cookie = `auth=true;samesite=strict;max-age=2678400`;
-          isAuth = true;
-          window.location.href = `http://${currentHost}/admin/table.html`;
-          break;
-        case getData(SERVER_URL).then((data) =>
-          checkObjectValues(data, 'login', usernameInputValue)):
-          usernameWarning.classList.remove('hidden');
-          usernameInput.value = '';
-          break;
-        case getData(SERVER_URL).then((data) =>
-          checkObjectValues(data, 'password', passwordInputValue)):
-          passwordWarning.classList.remove('hidden');
-          passwordInput.value = '';
-          break;
-        default:
-          // usernameWarning.classList.remove('hidden');
-          // passwordWarning.classList.remove('hidden');
-          // usernameInput.value = '';
-          // passwordInput.value = '';
-          break;
-      } */
     }
 
     if (cookie.auth === 'true') isAuth = true;
@@ -98,7 +59,7 @@ export const authFunc = () => {
       window.location.href = `http://${currentHost}/admin/index.html`;
     }
 
-    if (currentURL === '/admin/index.html' || currentURL === '/admin/' && isAuth) {
+    if ((currentURL === '/admin/index.html' || currentURL === '/admin/') && isAuth) {
       window.location.href = `http://${currentHost}/admin/table.html`;
     }
 
